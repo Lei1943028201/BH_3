85707d84c2039314cbbae622595d8bd9be27ce25
Lei1943028201 <1943028201@qq.com>
2019-05-07 22:10:19 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        hello
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import Mock from 'mockjs'
+    Mock.mock('/news/api','get',{
+        data: '你好'
+    })
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        created(){
+            axios.get('/news/api').then((res)=>{
+                console.log(res);
+            })
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
