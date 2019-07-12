eececaf9c786be170d1e23a1d824a4544ec77671
yanlei-1 <6789@jkl>
2019-07-12 18:40:37 +0800

    搭建基本结构

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import store from './store/'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI, {size: 'mini'});
+Vue.use(ElementUI, {size: 'small'});
 
 Vue.config.productionTip = false
 
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,6 +1,21 @@
 import {asyncRouterMap, constantRouterMap} from '@/router'
 
 /**
+ * 通过meta.role判断是否与当前用户权限匹配
+ * @param roles
+ * @param route
+ */
+function hasPermission(roles, route) {
+  // roles为权限身份数组
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.indexOf(role) >= 0)
+  } else {
+    return true
+  }
+}
+
+
+/**
  * 递归过滤异步路由表，返回符合用户角色权限的路由表
  * @param asyncRouterMap
  * @param roles
